f61b20ab013a27a0b0c0369e4d1a06b4b01cc2b0
Romawertq <plitynplitin10@gmail.com>
2026-03-22 20:45:54 +0300

    Небольшой код, а также добавление кода отправки и приёма

--- src/main/java/com/romawertq/messenger/HelloApplication.java
+++ src/main/java/com/romawertq/messenger/HelloApplication.java
@@ -1,8 +1,14 @@
 package com.romawertq.messenger;
 
+import com.romawertq.messenger.ReceiptAndDispatch.Dispatch;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +16,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Label lbl = new Label();
+        TextField textField = new TextField();
+        textField.setPrefColumnCount(11);
+        Button btn = new Button("Click");
+        btn.setOnAction(event -> lbl.setText("Сообщение отправлено"));
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, textField, btn, lbl);
+        Scene scene = new Scene(root, 250, 200);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module com.romawertq.messenger {
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
+    requires com.rabbitmq.client;
+    requires org.json;
+    requires com.fasterxml.jackson.databind;
 
     opens com.romawertq.messenger to javafx.fxml;
     exports com.romawertq.messenger;
